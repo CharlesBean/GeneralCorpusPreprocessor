6756d5d46df5e40f19447f9bb2079a922a579f6c
CharlesBean <beanchar@msu.edu>
2015-02-19 03:03:11 -0500

    Testing database connections and teaching myself

--- src/main.groovy
+++ src/main.groovy
@@ -1,3 +1,15 @@
+import groovy.sql.Sql
+
 /**
  * Created by charles on 2/19/15.
  */
+
+// Doing some testing...
+
+// TODO - add driver??
+
+def databaseWebMD = [url: "jdbc:mysql://localhost:3306/WebMD1", user: "root", password: ""]
+def sql = Sql.newInstance(databaseWebMD.url, databaseWebMD.user, databaseWebMD.password)
+
+def rows = sql.rows("SELECT * FROM WebMD1.diabetes_exchange LIMIT 10;")
+println rows.join('\n')
